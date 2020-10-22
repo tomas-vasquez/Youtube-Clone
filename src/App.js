04c8c745cfec8bb60a525fc380b2db623573b0c5
tomasdetloging <tomasdetloging@gmail.com>
2020-10-22 06:19:57 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Dashboard from './components/Dashboard/dashboard'
+class App extends Component {
+  render()
+  {
+    return (
+      <Dashboard/>
+      )
+
+  }
+  
+}
+
+export default App;
