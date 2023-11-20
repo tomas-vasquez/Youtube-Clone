2df652380d87fa616b123757da0df198df066ad1
Tomas Vasquez <tomasdetloging@gmail.com>
2023-11-20 10:35:25 -0500

    update packages

--- src/components/Dashboard/api.js
+++ src/components/Dashboard/api.js
@@ -1,15 +1,15 @@
-
-const API_KEY = "AIzaSyB7w-7NF3fo-wSHiQJote_FgSyQxOuJLZI";
+const API_KEY = "AIzaSyBzZO4iVx9mD-Q2NbW9MTg3tFX8bf0wD7U";
 
 export const getTrendingVideos = async () => {
   const response = await fetch(
-    `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=15&regionCode=IN&key=${API_KEY}`
+    `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=15&regionCode=BO&key=${API_KEY}`
   );
   const data = response.json();
+  console.log(data);
   return data;
 };
 
-export const getSearchResult = async query => {
+export const getSearchResult = async (query) => {
   const response = await fetch(
     `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&order=viewCount&q=${query}&type=video&videoDefinition=high&key=${API_KEY}`
   );
